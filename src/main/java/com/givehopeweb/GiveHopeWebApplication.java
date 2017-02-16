213b5588ea9a2627d66aa85e6494590c886b33e1
rubenvarela <rubenv6776@gmail.com>
2017-02-16 11:24:20 -0600

    Initial commit.

---
+++ src/main/java/com/givehopeweb/GiveHopeWebApplication.java
@@ -0,0 +1,12 @@
+package com.givehopeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveHopeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveHopeWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/givehopeweb/GiveHopeWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.givehopeweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiveHopeWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
