c9072a3be5f5df0c46ed482546ea8457f71aa4ef
David Ryan Alviola <david.alviola@gmail.com>
2017-02-17 15:38:17 -0600

    Added models. Relationships not set. Added properties.dist file.

---
+++ src/main/java/com/givehopeweb/models/User.java
@@ -0,0 +1,89 @@
+package com.givehopeweb.models;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by David on 2/17/17.
+ */
+@Entity
+@Table (name = "users")
+public class User {
+
+    public User () {
+
+    }
+
+    public User (User user) {
+
+        id = user.id;
+        username = user.username;
+        email = user.email;
+        password = user.password;
+        profilePicture = user.profilePicture;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Cannot be blank")
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank (message = "Cannot be blank")
+    @Email(message = "Enter a valid email")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank (message = "Cannot be blank")
+    @Size(min = 8, message = "Must be at least 8 characters")
+    private String password;
+
+    @Column
+    private String profilePicture;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
