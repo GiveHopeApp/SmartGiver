33feb21171afa59e632177018532f82a255b9e79
David Ryan Alviola <david.alviola@gmail.com>
2017-02-21 11:48:08 -0600

    Added migration file. Changed mapped by value for favoriteCharities column to user.

--- src/main/resources/static/sql/migration.sql
+++ src/main/resources/static/sql/migration.sql
@@ -1,5 +1,5 @@
 #Run these scripts to create the database and the user we will be using. Make sure your mySQL
-# server is running. Remeber to update your applications.properties file.
+# server is running. Remember to update your applications.properties file.
 
 CREATE DATABASE IF NOT EXISTS give_hope_db;
 
