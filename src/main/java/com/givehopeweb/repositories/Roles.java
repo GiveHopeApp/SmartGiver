feffd0896673de0692609f9a81e049cd3470b87f
David Ryan Alviola <david.alviola@gmail.com>
2017-02-21 15:51:25 -0600

    Imported UserRole class into Roles repository.

--- src/main/java/com/givehopeweb/repositories/Roles.java
+++ src/main/java/com/givehopeweb/repositories/Roles.java
@@ -1,5 +1,6 @@
 package com.givehopeweb.repositories;
 
+import com.givehopeweb.models.UserRole;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
