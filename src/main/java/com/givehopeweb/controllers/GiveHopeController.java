e1201974a53d9f4802361fc0e22b6bf8b31913ef
David Ryan Alviola <david.alviola@gmail.com>
2017-02-22 10:28:50 -0600

    Added controller for landing page.

---
+++ src/main/java/com/givehopeweb/controllers/GiveHopeController.java
@@ -0,0 +1,16 @@
+package com.givehopeweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by David on 2/22/17.
+ */
+@Controller
+public class GiveHopeController {
+
+    @GetMapping ("/")
+    public String showLandingPage () {
+        return "/home-screen";
+    }
+}
