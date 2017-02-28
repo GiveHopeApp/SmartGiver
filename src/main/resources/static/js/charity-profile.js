e8b9f09ebfbeccf94216acbaa5ed252c9dea2d51
rubenvarela <rubenv6776@gmail.com>
2017-02-28 14:24:52 -0600

    Completed buttons for donation amount, only need to complete the submit button.

--- src/main/resources/static/js/charity-profile.js
+++ src/main/resources/static/js/charity-profile.js
@@ -2,8 +2,37 @@
  * Created by rubenvarela on 2/28/17.
  */
 $(document).ready(function () {
-    $('.donate-wrapper > button').click(function() {
-        $('.donate-wrapper > button').removeClass('active');
-        $(this).addClass('active')
+    $('.button').click(function() {
+
+        $('.button').removeClass('active');
+        $(this).addClass('active');
+
+        $('input').val($(this).val());
+
+
+        console.log($('input').val());
     });
+
+    var clicked = false;
+
+    $('.button').last().click(function() {
+        if (clicked == false) {
+            $(this).css('transform', 'translateY(-9px)');
+            $('.button').css('display', 'none');
+            $(this).css('display', 'inline-block');
+            $(this).text('X');
+            $('input').css('display', 'inline-block');
+            $('.dollar').css('display', 'inline-block');
+            clicked = true
+        } else {
+            $(this).css('transform', 'none');
+            $('input').css('display', 'none');
+            $('.button').css('display', 'inline-block');
+            $(this).text('Custom');
+            $('.button').removeClass('active');
+            $('.dollar').css('display', 'none');
+            $('.button:nth-of-type(2)').addClass('active');
+            clicked = false
+        }
+    })
 });
