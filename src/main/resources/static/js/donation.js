d6511785f874cd9ed5753a0eed16cf419eb54f45
rubenvarela <rubenv6776@gmail.com>
2017-03-06 10:57:38 -0600

    Completed thank you page. Fixed navbar image and and profile button.

--- src/main/resources/static/js/donation.js
+++ src/main/resources/static/js/donation.js
@@ -11,8 +11,11 @@
     //Event listener for donate button
     $("#donate_btn").click(function() {
 
+        $(".overlay").fadeIn();
+
         Panda.on('success', function(cardToken) {
 
+            $(".overlay").fadeOut();
             // You now have a token you can use to refer to that credit card later.
             // This token is used in PandaPay API calls for creating donations and grants
             // so that you don't have to worry about security concerns with dealing with
@@ -27,18 +30,19 @@
 
         Panda.on('error', function(errors) {
 
+            $(".overlay").fadeOut();
             // errors is a human-readable list of things that went wrong
             //  (invalid card number, missing last name, etc.)
             console.log(errors);
 
 
-            var error;
-            for (var i = 0; i <= 6; i++) {
-                error = errors[i];
-                if (error[0] == "month") {
-                    console.log('test')
-                }
-            }
+            // var error;
+            // for (var i = 0; i <= 6; i++) {
+            //     error = errors[i];
+            //     if (error[0] == "month") {
+            //         console.log('test')
+            //     }
+            // }
         });
     })
 
