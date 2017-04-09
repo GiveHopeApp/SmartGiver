cfd60e666c32e97b26a3cc930f55347046125011
David Ryan Alviola <david.alviola@gmail.com>
2017-04-09 09:01:03 -0500

    Added documentation to all interfaces.

--- src/main/java/com/givehopeweb/repositories/Donations.java
+++ src/main/java/com/givehopeweb/repositories/Donations.java
@@ -7,11 +7,21 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 /**
- * Created by David on 2/21/17.
+ * <p>The <code>Donations</code> interface provides other classes with methods to query the
+ * donations table in the database. Spring Hibernate naming conventions are used for the methods
+ * to auto-generate the SQL commands.</p>
+ *
+ * @author David Ryan Alviola
+ * @since March 2017
  */
 public interface Donations extends CrudRepository <Donation, Integer> {
 
-        @Query("select d from Donation d where d.user.id like ?1")
-        List<Donation> findByUserId (int userId);
+    /**
+     * <p>Searches the database for donations belonging to the same user.</p>
+     *
+     * @param userId integer value representing the user ID
+     * @return list of <code>Donation</code> objects that belong to the same user
+     */
+    List<Donation> findByUserId (int userId);
 
 }
--- src/main/java/com/givehopeweb/repositories/Users.java
+++ src/main/java/com/givehopeweb/repositories/Users.java
@@ -4,10 +4,21 @@ import com.givehopeweb.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- * Created by David on 2/21/17.
+ * <p>The <code>Users</code> interface provides other classes with methods to query the
+ * users table in the database. Spring Hibernate naming conventions are used for the methods
+ * to auto-generate the SQL commands.</p>
+ *
+ * @author David Ryan Alviola
+ * @since March 2017
  */
 public interface Users extends CrudRepository<User, Integer>{
 
+    /**
+     * <p>Searches the database for a user by username</p>
+     *
+     * @param username String value representing the username
+     * @return <code>User</code> object with matching username
+     */
     User findByUsername (String username);
 
 }
